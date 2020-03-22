3a1c3ac7cc4ba51639f450c29377028f151e85bd
Nelyne <nkeenendu@gmail.com>
2020-03-22 21:24:09 +0100

    first task file added

---
+++ JSTask/index.js
@@ -0,0 +1,17 @@
+let name = "Nelyne";
+console.log(name);
+
+const courses = ['HTML', 'CSS', 'JavaScript'];
+
+for (let i=0; i<=200; i++) {
+
+    if (i%2===1) {
+ 
+      courses.push(i);
+ 
+    }
+ 
+  }
+ console.log(courses);
+
+
